030fb94eb6aab4cdf95567a02e356d72feb66ae9
essi4 <essi7688@gmail.com>
2026-09-23 23:37:24 +0330

    fix(next): adapt Next ESLint config to flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,23 @@
-import nextConfig from "eslint-config-next/core-web-vitals.js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-export default nextConfig;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
